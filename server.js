d9056014e27ec48cbb42c27c61ba83dd1b7ec705
Jérémy Choisy <choisy.jeremy@gmail.com>
2020-01-13 22:43:29 +0100

    fix CORS PATCH

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ server.use(passport.initialize());
 server.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH");
     next();
 });
 
